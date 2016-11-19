2cb5cb7918e7067bcd8bb106b9a552b9d8ef1a92
adenoble <txtremmy@gmail.com>
2016-11-19 02:07:05 +0000

    Upload

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SofTechBlendUni.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace SofTechBlendUni.DAL
+{
+    public class SchoolContext:DbContext
+    {
+
+        public SchoolContext(): base("SchoolContext")
+        { 
+        }
+            public DbSet<Student> Student {get; set;}
+            public DbSet<Course> Course {get; set;}
+            public DbSet<Enrollment> Enrollment{get; set;}
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+        }
+        
+        }
+
+
+    }
